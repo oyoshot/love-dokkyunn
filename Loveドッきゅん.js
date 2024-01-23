63e057b0e7d0b96e013818e8fba991987b7f1b5b
oyoshot <oyoshot@gmail.com>
2024-01-23 22:53:18 +0900

    Love ドッきゅん

---
+++ Loveドッきゅん.js
@@ -0,0 +1,39 @@
+const main = async (リサ, callback) => {
+  const [is不器用な僕さ, setIs不器用な僕さ] = useState(false);
+
+  const ぶっちゃけチューしてギュッと抱きしめたい = async (love) => {
+    love.Love();
+  };
+  const すべてすべてを投げ出したい = async (ドッきゅん) => {
+    ドッきゅん.ドッきゅん();
+  };
+  const どんなにどんなにどんなに欲しくても = async (love) => {
+    love.Love();
+  };
+  const 超えちゃ超えちゃいけないんだよ = async (ドッきゅん) => {
+    ドッきゅん.ドッきゅん();
+  };
+
+  Promise.all([
+    ぶっちゃけチューしてギュッと抱きしめたい(love),
+    すべてすべてを投げ出したい(ドッきゅん),
+    どんなにどんなにどんなに欲しくても(love),
+    超えちゃ超えちゃいけないんだよ(ドッきゅん),
+  ]).then(() => {
+    setIs不器用な僕さ(!is不器用な僕さ);
+  });
+
+  if (is不器用な僕さ) {
+    await callback.だけど大きな夢がある(
+      (order = 1),
+      (to = リサ),
+      (callback = もう一本),
+      (opt = { ...どどすこすこすこ }),
+    );
+  }
+
+  callback.Loveドッきゅん();
+};
+
+const l = new Listener();
+l.ListenAndServe(main((リサ = "I love you"), (callback = 僕)));
